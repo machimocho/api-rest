55f0ec95e863334f78f2b433a29320bb1f461140
Mario Alvarez <machimocho@gmail.com>
2016-11-25 22:29:46 -0600

    Node.js API REST Tutorial #10 - Implementación de DELETE y UPDATE de recursos
    https://youtu.be/CjOMThGjiMo

--- index.js
+++ index.js
@@ -50,12 +50,28 @@ app.post('/api/product',(req, res) => {
   })
 })
 
-app.put('/api/product:productId',(req, res) => {
+app.put('/api/product/:productId',(req, res) => {
+  let productId = req.params.productId
+  let update = req.body
 
+  Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+    if (err) res.status(500).send({message: `Error al actualizar el producto ${err}`})
+
+    res.status(200).send({product:productUpdated})
+  })
 })
 
-app.delete('/api/product/productId',(req, res) => {
+app.delete('/api/product/:productId',(req, res) => {
+  let productId = req.params.productId
 
+  Product.findById(productId, (err, product) => {
+    if (err) res.status(500).send({message: `Error al borrar el producto ${err}`})
+
+    product.remove(err => {
+      if (err) res.status(500).send({message: `Error al borrar el producto ${err}`})
+      res.status(200).send({message:`El producto ha sido eliminado`})
+    })
+  })
 })
 
 mongoose.connect('mongodb://localhost:27017/shop',(err, res) => {
